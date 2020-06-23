bb057b721f8d6846cafe71a78a566e19c276dd74
Ben Konyi <bkonyi@google.com>
2020-06-23 12:42:48 -0700

    Fix attach integration tests

--- packages/flutter_tools/lib/src/resident_runner.dart
+++ packages/flutter_tools/lib/src/resident_runner.dart
@@ -196,6 +196,7 @@ class FlutterDevice {
     ReloadMethod reloadMethod,
     GetSkSLMethod getSkSLMethod,
     PrintStructuredErrorLogMethod printStructuredErrorLogMethod,
+    bool disableDds = false,
   }) {
     final Completer<void> completer = Completer<void>();
     StreamSubscription<void> subscription;
@@ -206,12 +207,12 @@ class FlutterDevice {
       globals.printTrace('Connecting to service protocol: $observatoryUri');
       isWaitingForVm = true;
       vm_service.VmService service;
-
-      await device.dds.startDartDevelopmentService(
-        observatoryUri,
-        false, // TODO(bkonyi): ipv6 support?
-      );
-
+      if (!disableDds) {
+        await device.dds.startDartDevelopmentService(
+          observatoryUri,
+          false, // TODO(bkonyi): ipv6 support?
+        );
+      }
       try {
         service = await connectToVmService(
           observatoryUri,
@@ -934,6 +935,7 @@ abstract class ResidentRunner {
   Future<void> exit() async {
     _exited = true;
     await shutdownDevtools();
+    await shutdownDartDevelopmentService();
     await stopEchoingDeviceLog();
     await preExit();
     await exitApp();
@@ -941,6 +943,7 @@ abstract class ResidentRunner {
 
   Future<void> detach() async {
     await shutdownDevtools();
+    await shutdownDartDevelopmentService();
     await stopEchoingDeviceLog();
     await preExit();
     appFinished();
@@ -1103,6 +1106,12 @@ abstract class ResidentRunner {
     );
   }
 
+  Future<void> shutdownDartDevelopmentService() async {
+    await Future.wait<void>(
+      flutterDevices.map<Future<void>>((FlutterDevice device) => device.device.dds.shutdown())
+    );
+  }
+
   @protected
   void cacheInitialDillCompilation() {
     if (_dillOutputPath != null) {
@@ -1155,6 +1164,7 @@ abstract class ResidentRunner {
         reloadSources: reloadSources,
         restart: restart,
         compileExpression: compileExpression,
+        disableDds: debuggingOptions.disableDds,
         reloadMethod: reloadMethod,
         getSkSLMethod: getSkSLMethod,
         printStructuredErrorLogMethod: printStructuredErrorLog,
